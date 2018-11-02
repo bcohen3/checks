b999aad812a24caad9779292b16dbbddfe49fb4d
bcohen3 <30941091+bcohen3@users.noreply.github.com>
2018-11-02 16:39:34 -0400

    Update __init__.py

--- Lab2_0/check50/__init__.py
+++ Lab2_0/check50/__init__.py
@@ -6,13 +6,3 @@ class Lab2_0(Checks):
 	def exists(self):
 		"""Lab2_0.cpp exists"""
 		self.require("Lab2_0.cpp")
-
-	@check("exists")
-	def compiles(self):
-		"""Lab2_0.cpp compiles"""
-		self.spawn("clang -o g++ Lab2_0.cpp -lcs50 -lm").exit(0)
-
-	@check("compiles")
-	def test_41_cents(self):
-		"""input of 0.41 yields output of 4"""
-		self.spawn("./a.out").stdin("1 1").stdout("Your horoscope sign is: Capricorn\n", "Your horoscope sign is: Capricorn\n").exit(0)
